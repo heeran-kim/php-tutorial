5041bae39929a61a49b994fd8212512b62158dbf
Heeran Kim <heerankim3@gmail.com>
2024-07-28 20:43:08 +1000

    5. Writing HTML

---
+++ 5_writing_html/site.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+   </head>
+   <body>
+
+       <?php
+       // echo "string";
+        echo "Hello World";
+
+        // echo "any valid HTML";
+        echo "<h1>Lani's Site</h1>";
+        echo "<hr>"; // horizontal row
+        echo "<p>This is my site</p>";
+
+        // php code is executed in order by php server
+        ?>
+
+   </body>
+</html>
